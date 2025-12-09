c32d3e75e3fb2427c001f6eb2fc888c0612d4326
Vlad Dincă <dincavlad01@gmail.com>
2025-12-09 11:55:28 +0200

    Will be able to play 1 game

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+const board = gameBoard();
+
+
 function gameBoard() {
   let game = [
     0,0,0,
@@ -18,18 +21,50 @@ function gameReset(board){
   board.setBoard([0, 0, 0, 0, 0, 0, 0, 0, 0]);
 }
 
-const board = gameBoard();
+function playGame(){
+  let turn = 1;
+  let cells = document.querySelectorAll(".cell");
+
+  cells.forEach((cell, index) =>{
+    cell.dataset.index = index;
+  })
+  
+  
+  console.log(cells);
+    cells.forEach((cell) =>{
+      cell.addEventListener(("click"), () =>{
+
+        let index = parseInt(cell.dataset.index);
+        if (board.getBoard()[index] !== 0) return;
+
+
+        if (turn === 1){
+          cell.textContent = "X";
+          board.setCell(index, turn);
+          turn = 2;
+        }
+        else{
+          cell.textContent = "O";
+          board.setCell(index, turn);
+          turn=1;
+        }
+        console.log(board.getBoard());
+
+        gameController(player1, player2, board);     
+    }) 
+  })
+}
 
 function gameController(player1, player2, board) {
-  board.setCell(0, 1);
-  board.setCell(1, 2);
-  board.setCell(2, 2);
-  board.setCell(3, 2);
-  board.setCell(4, 1);
-  board.setCell(5, 1);
-  board.setCell(6, 1);
-  board.setCell(7, 2);
-  board.setCell(8, 2);
+  // board.setCell(0, 1);
+  // board.setCell(1, 2);
+  // board.setCell(2, 2);
+  // board.setCell(3, 2);
+  // board.setCell(4, 1);
+  // board.setCell(5, 1);
+  // board.setCell(6, 1);
+  // board.setCell(7, 2);
+  // board.setCell(8, 2);
   let currentBoard = board.getBoard();
   console.log(currentBoard);
 
@@ -58,21 +93,23 @@ function gameController(player1, player2, board) {
 
           console.log("Player 2 has won")
           gameReset(board);
-          console.log(board.getBoard());
         }
     else if(!currentBoard.includes(0)){
       console.log("It's a draw");
       gameReset(board);
-      console.log(board.getBoard());
     }
-      
-
-  console.log(board.getBoard());
   return board.getBoard();
+}
+
+function showModal(){
+  
 }
 
 let player1 = { name: "Vlad" };
 let player2 = { name: "Dinca" };
 
-gameController(player1, player2, board);
+showModal()
+
+
+playGame();
 console.log(board.getBoard()); 
